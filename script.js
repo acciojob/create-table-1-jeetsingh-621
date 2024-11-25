ed8e50244f1c39aa56f9748359c9ea051b496928
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-25 10:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function insert_Row() {
 	let cell1 = newrow.insertCell(0);
 	let cell2 = newrow.insertCell(1);
 
-	cell1.textContent = "New Cell 1"
-	cell2.textContent = "New Cell 2"
+	cell1.textContent = "New Cell1"
+	cell2.textContent = "New Cell2"
   
 }
